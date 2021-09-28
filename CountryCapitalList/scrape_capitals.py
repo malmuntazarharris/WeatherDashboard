6fa8eaaafb78f76caff7efbdafd3eb94033621ed
Malcolm Almuntazar-Harris <almuntazar.harris.malcolm@gmail.com>
2021-09-27 21:31:35 -0400

    Refactor scrape_capitals.py

--- CountryCapitalList/scrape_capitals.py
+++ CountryCapitalList/scrape_capitals.py
@@ -1,61 +1,39 @@
-import requests
-from bs4 import BeautifulSoup
-import re
 import pandas as pd
 import numpy as np
-import openpyxl
 
-CAPITAL_TABLE_INDEX = 1 # the second table on the wikipedia page contains the capitals
+def main():
+    URL = 'https://en.wikipedia.org/wiki/List_of_national_capitals'
 
-html = requests.get('https://en.wikipedia.org/wiki/List_of_national_capitals').content 
+    capital_df = pd.read_html(URL)[1]
 
-soup = BeautifulSoup(html, 'lxml')
+    # rename columns
+    capital_df.rename(columns={'City/Town': 'Capital'}, inplace=True)
 
-capitals_table = soup.findAll('table')[CAPITAL_TABLE_INDEX]
+    # drop the notes
+    capital_df.drop(columns='Notes', inplace=True)
 
-capitals_rows = capitals_table.findAll('tr') # find all rows in the table
+    # some of the webscraped lists have '\n' and other footnotes in the country 
+    capital_df.loc[capital_df['Country/Territory'].str.contains('\n').fillna(False), 'Country/Territory'] = np.nan
+    capital_df['Country/Territory'].ffill(inplace=True)
 
-# TODO: refactor below block, maybe replace the bool with the row span regex check???
-sovereign_rows = []
-two_capitals = False #bool to flag if a country has two capitals
-for tr in capitals_rows:
-    # only the bolded rows should be added, bolded capitals indicate that it's a sovereign state and/or recognized by the UN
-    # also if two_capitals are set to True that means that the row it's iterating is the second capital of a country
-    if len(tr.findAll('b')) > 0 or two_capitals == True: 
-        if two_capitals == True: 
-            two_capitals = False  
-        bolded_row = []
-        if len(re.findall(r'rowspan="[0-255]"', str(tr))) > 0: #rowspans indicate that a country has two capitals (e.g. Malaysia), set two_capitals bool to true if row contains rowspan
-            two_capitals = True
-        td = tr.find_all('td')
-        for i in td:
-            bolded_row.append(i.text) # add each cell to a list
-        sovereign_rows.append(bolded_row) # add list representing row to the rows of sovereign nations
+    # remove all the text in parentheses and brackets
+    capital_df['Country/Territory'] = capital_df['Country/Territory'].str.replace(r"\(.*\)", '', regex=True)
+    capital_df['Capital'] = capital_df['Capital'].str.replace(r"\(.*\)", '', regex=True)
+    capital_df['Country/Territory'] = capital_df['Country/Territory'].str.replace(r"\[.*?\]", '', regex=True)
+    capital_df['Capital'] = capital_df['Capital'].str.replace(r"\[.*?\]", '', regex=True)
 
-# convert soveign_rows (list of lists) into dataframe
-sovereign_df = pd.DataFrame(sovereign_rows, columns=['Capital', 'Country', 'Footnote'])
+    # remove reverse single quotation marks
+    capital_df['Capital'] = capital_df['Capital'].str.replace("ʻ", "'", regex=False)
 
-# drop the footnotes
-sovereign_df = pd.DataFrame(sovereign_rows, columns=['Capital', 'Country', 'Footnote'])
-sovereign_df.drop(columns='Footnote', inplace=True)
+    # remove the whitespaces in all strings of the dataframe
+    capital_df_obj = capital_df.select_dtypes(['object'])
+    capital_df[capital_df_obj.columns] = capital_df_obj.apply(lambda x: x.str.strip())
 
-# some of the webscraped lists have '\n' and other footnotes in the country 
-sovereign_df.loc[sovereign_df['Country'].str.contains('\n').fillna(False), 'Country'] = np.nan
-sovereign_df['Country'].ffill(inplace=True)
+    # fix specific issue with capital of Palau: Ngerulmud does not have data in the openweather api, however the former capital Koror does
+    capital_df["Capital"].replace('Ngerulmud', 'Koror', inplace=True, regex=False)
 
-# remove all the text in parentheses
-sovereign_df['Country'] = sovereign_df['Country'].str.replace(r"\(.*\)", '', regex=True)
-sovereign_df['Capital'] = sovereign_df['Capital'].str.replace(r"\(.*\)", '', regex=True)
+    # output an excel file of the dataframe
+    capital_df.to_csv('C:/Users/Malcolm/Weather/WeatherDashboard/CountryCapitalList/countries_capitals.csv', index=False, encoding='utf-8-sig')
 
-# remove reverse single quotation marks
-sovereign_df['Capital'] = sovereign_df['Capital'].str.replace("ʻ", "'", regex=False)
-
-# remove the whitespaces in all strings of the dataframe
-sovereign_df_obj = sovereign_df.select_dtypes(['object'])
-sovereign_df[sovereign_df_obj.columns] = sovereign_df_obj.apply(lambda x: x.str.strip())
-
-# fix specific issue with capital of Palau: Ngerulmud does not have data in the openweather api, however the former capital Koror does
-sovereign_df["Capital"].replace('Ngerulmud', 'Koror', inplace=True, regex=False)
-
-# output an excel file of the dataframe
-sovereign_df.to_excel('C:/Users/Malcolm/Weather/WeatherDashboard/CountryCapitalList/countries_capitals.xlsx', index=False)
+if __name__ == "__main__":
+    main()
